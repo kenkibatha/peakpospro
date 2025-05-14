2573a83d8f928488c00addd4a1c19c6533ffcec9
kenkibatha <kenneth.kibatha@gmail.com>
2025-05-14 14:37:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const navLinks= document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+
+//show mobile menu on click
+menuOpenButton.addEventListener("click", () => {    
+    document.body.classList.toggle("show-mobile-menu");
+});
+//close mobile menu on click of close button
+menuCloseButton.addEventListener("click", () => menuOpenButton.click
+());
+//close mobile menu on click of any link button on nav bar
+navLinks.forEach(link =>{
+    link.addEventListener("click",()=> menuOpenButton.click());
+
+});
+
+
+//document.addEventListener('DOMContentLoaded', () => {
+//initialize swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    spaceBetween: 25,
+    grabCursor: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1  
+        },
+    
+        768: {
+            slidesPerView: 2  
+        },
+ 
+        1024: {
+            slidesPerView: 3
+        }
+    }
+  });
+//});
